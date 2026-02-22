eca53a2bf728d738986324837afdb02bacd854c0
Jeremiah Tesfaye <jeremiahtesfaye3@gmail.com>
2026-02-22 16:08:04 +0300

    feat: bootstrap NestJS app with AppModule

---
+++ src/main.ts
@@ -0,0 +1,27 @@
+import { ValidationPipe } from '@nestjs/common';
+import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+    }),
+  );
+
+  const config = new DocumentBuilder()
+    .setTitle('Course API')
+    .setDescription('Backend internship challenge - Course management API')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, document);
+
+  await app.listen(process.env.PORT ?? 3000);
+}
+bootstrap();
